5c71deb4f075ac01e54238a94af6398bdbc3134a
Elfana, A | Elfaael <elfaael12@gmail.com>
2024-02-26 20:49:05 +0700

    Initial Commit

---
+++ iSwiftBiometric/iSwiftBiometricApp.swift
@@ -0,0 +1,17 @@
+//
+//  iSwiftBiometricApp.swift
+//  iSwiftBiometric
+//
+//  Created by Elfana Anamta Chatya on 26/02/24.
+//
+
+import SwiftUI
+
+@main
+struct iSwiftBiometricApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
